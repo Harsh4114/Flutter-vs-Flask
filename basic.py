6034fd4ce55e26611aa5c5dcc2edd3c1b670b6fd
Harsh4114 <71206223+Harsh4114@users.noreply.github.com>
2023-11-22 21:18:34 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,34 +1,41 @@
+# import part 
+
 import flet
 from flet import Page, TextField, Row, IconButton, icons
 
-
+#main box , all variable , define here 
 def main(page: Page):
     page.title = "Counter App"
     page.vertical_alignment = "center"
 
-    txt_field = TextField(value="0", width=100, text_align="right")
+    txt_field = TextField(value="0", width=100, text_align="right") # value = 0 -  starting of  value 
     
-
+# other func. 
     def minus_clicked(e):
         txt_field.value = int(txt_field.value) - 1
-        page.update()
+        page.update() # page update use for update value or layout , other ..... 
 
     def plus_clicked(e):
         txt_field.value = int(txt_field.value) + 1
-        page.update()
+        page.update()# page update use for update value or layout , other ..... 
 
+
+# page layout start
     page.add(
-        Row(
+     # Body Part : Row is Element 
+
+        Row( # Row start 
+            # Row having children  ( 1 or more ) ,  must written in [all child] , child seperate by ( , ) 
             [
-
-                
-                IconButton(icons.REMOVE, on_click=minus_clicked),
-                txt_field,
-                IconButton(icons.ADD, on_click=plus_clicked),
+                IconButton(icons.REMOVE, on_click=minus_clicked), # child 1 , minus_clicked - func . call  
+                txt_field,                                        # child 2 , this is variable method child 
+                IconButton(icons.ADD, on_click=plus_clicked),     # child 3 , plus_clicked - func . call 
             ],
-            alignment="center",
-        )
+           
+            alignment="center", # Row Position 
+        ) # Row End
+        
     )
-
-
-flet.app(target=main, view=flet.WEB_BROWSER)
+# page layout end 
+# page type 
+flet.app(target=main, view=flet.WEB_BROWSER) # PAGE TYPE END 
